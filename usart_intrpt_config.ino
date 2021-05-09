96d962ae4269a1fa1b07a218be6f792843446f19
V-HEGDE <46925020+V-HEGDE@users.noreply.github.com>
2021-05-09 16:27:29 +0530

    Add files via upload

--- usart_intrpt_config.ino
+++ usart_intrpt_config.ino
@@ -1,18 +1,32 @@
-byte rx;
+char rx;
+unsigned long var_l='a';
 void setup() {
+ var_l='a';
+  var_l|=('b'<<8);
+   var_l|=(unsigned long)'c'<<16;
+   var_l|=(unsigned long)'d'<<24;
   // put your setup code here, to run once:
 UBRR0H=0X00;
 UBRR0L=103;
 
 UCSR0B=(1<<TXEN0)|(1<<RXEN0)|(1<<RXCIE0);
-UCSR0C=(1<<UCSZ00)|(1<<UCSZ01);
+UCSR0C=(1<<UCSZ00)|(1<<UCSZ01);;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+}
+void sendchar(unsigned char *c){
+  int i;
+  rx=0;
+  for(i=0;i<4;i++){
+    UDR0=*c;
+    c++;
+    while(!(UCSR0A&(1<<5)));
+  }
 }
 ISR(USART_RX_vect){
-  rx=UDR0+1;
-  UDR0=rx-1;
+  rx=UDR0;
+  if(rx==1){
+  sendchar((unsigned char*)&var_l);
+  }
 }
